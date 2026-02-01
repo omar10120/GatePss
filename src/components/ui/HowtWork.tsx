fb17b2dd2aa787f9393be8b78fbc43ec53835824
tamer tamer <amr.dawoodi@hotmail.com>
2026-02-01 15:01:28 +0300

    fix video , add play button (prevent preload until user click play button )

--- src/components/ui/HowtWork.tsx
+++ src/components/ui/HowtWork.tsx
@@ -38,7 +38,7 @@ export default function HowtWork() {
                     <div className="mb-12 md:mb-16 max-w-5xl mx-auto">
                         <div className="bg-gray-200 rounded-2xl shadow-sm border border-gray-300 p-6 md:p-8 lg:p-12 aspect-video flex items-center justify-center">
                             <div className="text-center">
-                               {howtoVideo ? <video controls src={howtoVideo} className="w-full h-full object-cover" preload="metadata" /> : 
+                               {howtoVideo ? <video controls src={howtoVideo} className="w-full h-full object-cover" preload="none" /> : 
                                  <svg className="w-16 h-16 md:w-20 md:h-20 mx-auto mb-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
                                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
