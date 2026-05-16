776fcb051c4b42cc15ea458220042cb8031bacb4
tamer tamer <amr.dawoodi@hotmail.com>
2026-05-16 13:58:58 +0300

    display request id insated of id in request table

--- src/app/[locale]/admin/requests/page.tsx
+++ src/app/[locale]/admin/requests/page.tsx
@@ -438,7 +438,10 @@ export default function AdminRequestsPage() {
                                                                             </svg>
                                                                         </button>
                                                                     </td>
-                                                                    <td className="px-2 sm:px-3 md:px-4 lg:px-6 py-3 md:py-4 text-[#747474] text-[12px] sm:text-[14px] text-center">{request.id}</td>
+                                                                    {/* <td className="px-2 sm:px-3 md:px-4 lg:px-6 py-3 md:py-4 text-[#747474] text-[12px] sm:text-[14px] text-center">{request.id}</td> */}
+                                                                    <td className="px-2 sm:px-3 md:px-4 lg:px-6 py-3 md:py-4 text-[#747474] text-[12px] sm:text-[14px] text-center">
+                                                                        {request.requestNumber ? request.requestNumber : request.id}
+                                                                    </td>
                                                                     <td className="px-2 sm:px-3 md:px-4 lg:px-6 py-3 md:py-4 text-[#222222] font-bold text-[12px] sm:text-[14px] text-center">{formatDate(request.createdAt)}</td>
                                                                     <td className="px-2 sm:px-3 md:px-4 lg:px-6 py-3 md:py-4 text-[#222222] font-bold text-[12px] sm:text-[14px] text-center">{request.applicantNameEn || request.applicantNameAr}</td>
                                                                     <td className="px-2 sm:px-3 md:px-4 lg:px-6 py-3 md:py-4 relative overflow-visible text-center">
